2ab34fb19925774d2a3bd7e4bad61cf0db51e79e
JackSharks05 <jack.dehaan@iCloud.com>
2022-01-30 18:16:00 -0500

    car-updates :)

--- CalendarTest WatchKit Extension/ContentView.swift
+++ CalendarTest WatchKit Extension/ContentView.swift
@@ -6,167 +6,71 @@
 //
 
 import SwiftUI
-var cycleDay : Int {
-    get {
-//        let date = Date()
-//        let calendar = Calendar.current
-//        var month = calendar.component(.month, from: date)
-//        var day = calendar.component(.day, from: date)
-//        if month >= 6 && day >= 8 {
-//            month = 6
-//            day = 8
-//        }
-//        let optionalCycle: Int? = dateToCycleDay[month-1][day]
-//        if let cycleDay = optionalCycle {
-//            return cycleDay
-//        } else { return 0 }
-        return 3
-        
+func getDate() -> String {
+    let date = Date()
+    let cal = Calendar.current
+    let month = cal.component(.month, from: date)
+    let day = cal.component(.day, from: date)
+    let year = cal.component(.year, from: date)
+    let weekday = cal.component(.weekday, from: date)
+    return "\(cal.shortWeekdaySymbols[weekday-1]), \(cal.shortMonthSymbols[month-1]) \(day), \(year)"
+}
+func getOrder() -> Text {
+if nowIsAfterBlock(block: 0){
+return (Text(order[cycleDay]![0]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![1]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![2]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![3]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![4]).foregroundColor(.red).fontWeight(.medium))
+} else if nowIsAfterBlock(block: 2) {
+return (Text(order[cycleDay]![0]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![1]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![2]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![3]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![4]).foregroundColor(.red).fontWeight(.medium))
+    } else if nowIsAfterBlock(block: 3){
+return (Text(order[cycleDay]![0]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![1]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![2]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![3]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![4]).foregroundColor(.red).fontWeight(.medium))
+} else if nowIsAfterBlock(block: 5){
+return (Text(order[cycleDay]![0]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![1]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![2]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![3]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![4]).foregroundColor(.red).fontWeight(.medium))
+} else if nowIsAfterBlock(block: 5){
+return (Text(order[cycleDay]![0]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![1]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![2]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![3]).foregroundColor(.blue).fontWeight(.light) + Text("-") + Text(order[cycleDay]![4]).foregroundColor(.blue).fontWeight(.light))
+} else if isAfter(hour1: getHour(), minute1: getMinute(), hour2: 8, minute2: 55){
+return (Text(order[cycleDay]![0]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![1]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![2]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![3]).foregroundColor(.red).fontWeight(.medium) + Text("-") + Text(order[cycleDay]![4]).foregroundColor(.red).fontWeight(.medium))
+} else {
+    return Text("")
+}
+}
+func getNextClass() -> String {
+    if cycleDay == 0{
+        return ""
+    } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 8,minute2: 55){
+        return ("Next: " + classes[cycleDay]![0])
+    } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 10,minute2: 00){
+        return ("Next: " + getMorningActivity())
+    } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 10,minute2: 35){
+        return ("Next: " + classes[cycleDay]![1])
+    } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 11,minute2: 25){
+        return ("Next: " + classes[cycleDay]![2])
+    } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 13,minute2: 20){
+        return ("Next: " + classes[cycleDay]![3])
+    } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 14,minute2: 30){
+        return ("Next: " + classes[cycleDay]![4])
+    } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 15,minute2: 15){
+        return ("Next: " + classes[cycleDay]![5])
+    } else {
+        return "Next: Go home!"
     }
 }
-var dateToCycleDay: [[Int: Int]] = [
-    // January
-    [1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:7, 25:8, 26:1, 27:2, 28:3, 29:0, 30:0, 31:4],
-    // February
-    [1:5, 2:6, 3:7, 4:8, 5:0, 6:0, 7:1, 8:2, 9:3, 10:4, 11:5, 12:0, 13:0, 14:7, 15:7, 16:8, 17:1, 18:0, 19:0, 20:0, 21:0, 22:2, 23:3, 24:4, 25:5, 26:0, 27:0, 28:6],
-    // March
-    [1:7, 2:8, 3:1, 4:2, 5:0, 6:0, 7:3, 8:4, 9:5, 10:6, 11:7, 12:0, 13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0, 25:0, 26:0, 27:0, 28:8, 29:1, 30:2, 31:3],
-    // April
-    [1:4, 2:0, 3:0, 4:5, 5:6, 6:7, 7:8, 8:1, 9:0, 10:0, 11:2, 12:3, 13:4, 14:5, 15:0, 16:0, 17:0, 18:6, 19:7, 20:8, 21:1, 22:2, 23:0, 24:0, 25:3, 26:4, 27:5, 28:6, 29:7, 30:0],
-    // May
-    [1:0, 2:8, 3:1, 4:2, 5:3, 6:4, 7:0, 8:0, 9:5, 10:6, 11:7, 12:8, 13:1, 14:0, 15:0, 16:2, 17:3, 18:4, 19:5, 20:6, 21:0, 22:0, 23:7, 24:8, 25:1, 26:2, 27:3, 28:0, 29:0, 30:0, 31:4],
-    // June
-    [1:5, 2:6, 3:7, 4:0, 5:0, 6:8, 7:1, 8:0]
-]
-var classes: [Int: [String]] = [
-    0: ["","","","",""],
-    1: ["Comp Sci (C)", "English (E)", "Physics (D)", "Free/OPI (A)", "Publ. Sp. (B)","Go Home!"],
-    2: ["Latin (F)", "Spanish (G)", "Precalc (H)", "Math Team (H)", "Publ. Sp. (B)","Go Home!"],
-    3: ["Comp Sci (C)", "Physics (D)", "Latin (F)", "English (E)", "Spanish (G)", "Fitness Center"],
-    4: ["Precalc (H)", "Free (A)", "Publ. Sp. (B)", "Comp Sci (C)", "Physics (D)","Go Home!"],
-    5: ["Spanish (G)", "Math Team (A)", "Precalc (H)", "English (E)", "Latin (F)","Go Home!"],
-    6: ["Publ. Sp. (B)", "Comp Sci (C)", "Physics (D)", "English (E)", "Latin (F)","Fitness Center"],
-    7: ["Free (A)", "Precalc (H)", "Spanish (G)", "Publ. Sp. (B)", "Comp Sci (C)","Go Home!"],
-    8: ["Physics (D)", "English (E)", "Latin (F)", "Spanish (G)", "Precalc (H)","Fitness Center"]
-]
-struct ContentView: View {
-    
-    func getDate() -> String {
-        let date = Date()
-        let cal = Calendar.current
-        let month = cal.component(.month, from: date)
-        let day = cal.component(.day, from: date)
-        let year = cal.component(.year, from: date)
-        return "\(cal.monthSymbols[month-1]) \(day), \(year)"
-    }
-    func getHour() -> Int{
-        let date = Date()
-        let cal = Calendar.current
-        let hour = cal.component(.hour, from: date)
-        return hour
-        
-    }
-    func getMinute() -> Int{
-        let date = Date()
-        let cal = Calendar.current
-        let minute = cal.component(.minute, from: date)
-        return minute
-    }
-    func getWeekday() -> String {
-        // TODO
-        let date = Date()
-        let cal = Calendar.current
-        let weekday = cal.component(.weekday, from: date)
-        return "\(cal.weekdaySymbols[weekday-1])"
-    }
-    
-    func getOrder() -> String {
-        return order[cycleDay]![0]
-    }
-    func getCycleDay() -> Int {
-        let date = Date()
-        let calendar = Calendar.current
-        var month = calendar.component(.month, from: date)
-        var day = calendar.component(.day, from: date)
-        if month >= 6 && day >= 8 {
-            month = 6
-            day = 8
-        }
-        let optionalCycle: Int? = dateToCycleDay[month-1][day]
-        if let cycleDay = optionalCycle {
-            return cycleDay
-        } else { return 0 }
-    }
-    func isAfter(hour1:Int,minute1: Int,hour2:Int ,minute2:Int) -> Bool{
-        if hour2>hour1{
-            return true
-        } else if hour1>hour2{
-            return false
-        } else {
-            return (minute2>minute1)
-        }
-        
-    }
-    func getMorningActivity() -> String {
-        // TODO
-        let date = Date()
-        let cal = Calendar.current
-        let weekday = cal.component(.weekday, from: date)
-        switch weekday {
-        case 1:
-            return "None"
-        case 2:
-            return "Community Meeting"
-        case 3:
-            return "Clubs"
-        case 4:
-            return "Advisory"
-        case 5:
-            return "Clubs"
-        case 6:
-            return "Class Meeting"
-        case 7:
-            return "None"
-        default:
-            return "error..."
-        }
-    }
-    func getNextClass() -> String {
-        if cycleDay == 0{
-            return ""
-        } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 8,minute2: 55){
-            return ("Next: " + classes[cycleDay]![0])
-        } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 10,minute2: 00){
-            return ("Next: " + getMorningActivity())
-        } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 10,minute2: 35){
-            return ("Next: " + classes[cycleDay]![1])
-        } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 11,minute2: 25){
-            return ("Next: " + classes[cycleDay]![2])
-        } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 13,minute2: 20){
-            return ("Next: " + classes[cycleDay]![3])
-        } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 14,minute2: 30){
-            return ("Next: " + classes[cycleDay]![4])
-        } else if isAfter(hour1: getHour(),minute1: getMinute(),hour2: 15,minute2: 15){
-            return ("Next: " + classes[cycleDay]![5])
-        } else {
-            return "Next: Go home!"
-        }
-    }
-    func getClasses() -> String {
-        if let classList = classes[cycleDay]?.joined(separator: "\n") {
-            return classList
-        } else { return "e" }
-    }
-    
-    var order: [Int: [String]] = [
-        0:["No classes today!"],
-        1:["C-E-D-A-B"], 2:["F-G-H-A-B"], 3:["C-D-F-E-G"], 4:["H-A-B-C-D"], 5:["G-A-H-E-F"], 6:["B-C-D-E-F"], 7:["A-H-G-B-C"], 8:["D-E-F-G-H"]
-    ]
+func getClasses() -> String {
+    if let classList = classes[cycleDay]?.joined(separator: "\n") {
+        return classList
+    } else { return "e" }
+}
 
+var order: [Int: [String]] = [
+    0:["----No school!","","","",""],
+    1:["C","E","D","A","B"], 2:["F","G","H","A","B"], 3:["C","D","F","E","G"], 4:["H","A","B","C","D"], 5:["G","A","H","E","F"], 6:["B","C","D","E","F"], 7:["A","H","G","B","C"], 8:["D","E","F","G","H"]
+]
+
+
+struct ContentView: View {
     
     var body: some View {
 
         VStack{
+            Spacer()
         Text("Day \(cycleDay)")
             .font(.title)
             .fontWeight(.heavy)
@@ -175,19 +79,19 @@ struct ContentView: View {
             
             Text (getNextClass()).fontWeight(.heavy)
             Text("\(getDate())")
-            Text("\(getWeekday())")
-                .multilineTextAlignment(.center)
-            .multilineTextAlignment(.center)
-            Text("\(getOrder())").multilineTextAlignment(.center)
+            
+            
+            getOrder()
             
             Spacer()
             Spacer()
             Spacer()
-                        
+            if isSchool() {
             NavigationLink(destination: DayView()){
                 Text("Today")
                     .fontWeight(.heavy)
                         }
+            }
         }
 
     }
@@ -198,3 +102,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
--- CalendarTest WatchKit Extension/DayView.swift
+++ CalendarTest WatchKit Extension/DayView.swift
@@ -8,67 +8,19 @@
 import SwiftUI
 
 struct DayView: View {
-//    @ObservedObject var dayManager = DayFetcher()
-    func getHour() -> Int{
-        let date = Date()
-        let cal = Calendar.current
-        let hour = cal.component(.hour, from: date)
-        return hour
-        
-    }
-    func getMinute() -> Int{
-        let date = Date()
-        let cal = Calendar.current
-        let minute = cal.component(.minute, from: date)
-        return minute
-    }
-    func getMorningActivity() -> String {
-        // TODO
-        let date = Date()
-        let cal = Calendar.current
-        let weekday = cal.component(.weekday, from: date)
-        switch weekday {
-        case 1:
-            return "None"
-        case 2:
-            return "Community Meeting"
-        case 3:
-            return "Clubs"
-        case 4:
-            return "Advisory"
-        case 5:
-            return "Clubs"
-        case 6:
-            return "Class Meeting"
-        case 7:
-            return "None"
-        default:
-            return "error..."
-        }
-    }
-    func isAfter(hour1:Int,minute1: Int,hour2:Int ,minute2:Int) -> Bool{
-        if hour2>hour1{
-            return true
-        } else if hour1>hour2{
-            return false
-        } else {
-            return (minute2>minute1)
-        }
-        
-    }
     var body: some View {
         ScrollView{
             VStack{
                 Group{
                     Text("08:55 - 09:55:").fontWeight(.bold)
-                    if isAfter(hour1: 9,minute1: 55,hour2: getHour(),minute2: getMinute()){
+                    if nowIsAfterBlock(block: 0){
                         Text(classes[cycleDay]![0]).foregroundColor(.blue).fontWeight(.light)}
                     else {
                         Text(classes[cycleDay]![0]).foregroundColor(.red).fontWeight(.medium)}
                     }
                 Spacer()
                 Text("10:00 - 10:30:").fontWeight(.bold)
-                if isAfter(hour1: 10,minute1: 30,hour2: getHour(),minute2: getMinute()){
+                if nowIsAfterBlock(block: 1){
                     Text(getMorningActivity()).foregroundColor(.blue).fontWeight(.light)}
                 else {
                     Text(getMorningActivity()).foregroundColor(.red).fontWeight(.medium)}
@@ -76,13 +28,13 @@ struct DayView: View {
                 }
                 Group{
                 Text("10:35 - 11:20:").fontWeight(.bold)
-                    if isAfter(hour1: 11,minute1: 20,hour2: getHour(),minute2: getMinute()){
+                    if nowIsAfterBlock(block: 2){
                         Text(classes[cycleDay]![1]).foregroundColor(.blue).fontWeight(.light)}
                     else {
                         Text(classes[cycleDay]![1]).foregroundColor(.red).fontWeight(.medium)}
                 Spacer()
                 Text("11:25 - 12:25:").fontWeight(.bold)
-                    if isAfter(hour1: 12,minute1: 25,hour2: getHour(),minute2: getMinute()){
+                    if nowIsAfterBlock(block: 3){
                         Text(classes[cycleDay]![2]).foregroundColor(.blue).fontWeight(.light)}
                     else {
                         Text(classes[cycleDay]![2]).foregroundColor(.red).fontWeight(.medium)}
@@ -91,7 +43,7 @@ struct DayView: View {
                 }
                 Group{
                 Text("12:25 - 13:15:").fontWeight(.bold)
-                    if isAfter(hour1: 13,minute1: 15,hour2: getHour(),minute2: getMinute()){
+                    if nowIsAfterBlock(block: 4){
                         Text("Lunch").foregroundColor(.blue).fontWeight(.light)}
                     else {
                         Text("Lunch").foregroundColor(.red).fontWeight(.medium)}
@@ -100,44 +52,26 @@ struct DayView: View {
                 }
                 Group{
                 Text("13:20 - 14:20:").fontWeight(.bold)
-                    if isAfter(hour1: 14,minute1: 20,hour2: getHour(),minute2: getMinute()){
+                    if nowIsAfterBlock(block: 5){
                         Text(classes[cycleDay]![3]).foregroundColor(.blue).fontWeight(.light)}
                     else {
                         Text(classes[cycleDay]![3]).foregroundColor(.red).fontWeight(.medium)}
                 Spacer()
                 Text("14:30 - 15:15:").fontWeight(.bold)
-                    if isAfter(hour1: 15,minute1: 15,hour2: getHour(),minute2: getMinute()){
+                    if nowIsAfterBlock(block: 6){
                         Text(classes[cycleDay]![4]).foregroundColor(.blue).fontWeight(.light)}
                     else {
-                        Text(classes[cycleDay]![5]).foregroundColor(.red).fontWeight(.medium)}
+                        Text(classes[cycleDay]![4]).foregroundColor(.red).fontWeight(.medium)}
+                    if (isSports()){
+                        Text("15:20 - 16:10:").fontWeight(.bold)
+                        if isAfter(hour1: 16,minute1: 10,hour2: getHour(),minute2: getMinute()){
+                            Text("Fitness Center").foregroundColor(.blue).fontWeight(.light)}
+                        else {
+                            Text("Fitness Center").foregroundColor(.red).fontWeight(.medium)}
+                    }
                 }
             }
-//        VStack{
-//            NavigationLink(destination: DayView1()){
-//                Text("Comp Sci")
-//                        }
-//            NavigationLink(destination: DayView2()){
-//                Text("Class Meeting")
-//                        }
-//            NavigationLink(destination: DayView3()){
-//                Text("Physics")
-//                        }
-//            NavigationLink(destination: DayView4()){
-//                Text("Latin")
-//                        }
-//            NavigationLink(destination: DayView5()){
-//                Text("Lunch")
-//                        }
-//            NavigationLink(destination: DayView6()){
-//                Text("English")
-//                        }
-//            NavigationLink(destination: DayView7()){
-//                Text("Spanish")
-//                        }
-//
-//
-//
-//        }
+
         }
     }
 
