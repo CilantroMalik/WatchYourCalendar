53fbb9b8f3369d68005eec8894a187f1508059f6
Rohan Malik <rohan_malik@ryecountryday.org>
2022-02-05 16:40:49 -0500

    Add view for scheduling an event

--- CalendarTest WatchKit Extension/SchedulingView.swift
+++ CalendarTest WatchKit Extension/SchedulingView.swift
@@ -6,21 +6,19 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 //func hasAssessment(block: Int) -> Bool {
 //    return false
 //}
-func meetingOrAssessment() -> String{
-    return isClass() ? "Assessment" : "Meeting"
-}
+
 func getAssessmentBlock() -> Int{
     return 0//fix
 }
-func isClass() -> Bool{ //isClass or free
-    true
-}
-@State var hasAs: [Int: [Bool]] = [
-    0: [false, false, false, false, false, false, false],//5 blocks + clubs and lunch
+
+var hasAs: [Int: [Bool]] = [
+    //  Blk 1  Clubs  Blk 2  Blk 3  Lunch  Blk 4  Blk 5
+    0: [false, false, false, false, false, false, false],
     1: [false, false, false, false, false, false, false],
     2: [false, false, false, false, false, false, false],
     3: [false, false, false, false, false, false, false],
@@ -30,16 +28,113 @@ func isClass() -> Bool{ //isClass or free
     7: [false, false, false, false, false, false, false],
     8: [false, false, false, false, false, false, false]
 ]
+
 struct SchedulingView: View {
+    var day: Int
+    var block: Int
+    
+    @State var isEvent = false {
+        didSet {
+            hasAs[day]![block] = isEvent
+        }
+    }
+    
+    func meetingOrAssessment() -> String{
+        switch block {
+        case 0:
+            return classes[day]![0].starts(with: "Free ") ? "Meeting" : "Assessment"
+        case 1:
+            return "Meeting"
+        case 2:
+            return classes[day]![1].starts(with: "Free ") ? "Meeting" : "Assessment"
+        case 3:
+            return classes[day]![2].starts(with: "Free ") ? "Meeting" : "Assessment"
+        case 4:
+            return "Meeting"
+        case 5:
+            return classes[day]![3].starts(with: "Free ") ? "Meeting" : "Assessment"
+        case 6:
+            return classes[day]![4].starts(with: "Free ") ? "Meeting" : "Assessment"
+        default:
+            return "e"
+        }
+    }
+    
+    func getPeriod(blockNum: Int) -> String {
+        switch blockNum {
+        case 0:
+            return blocks[day]![0] + " Block"
+        case 1:
+            return "Break/Clubs"
+        case 2:
+            return blocks[day]![1] + " Block"
+        case 3:
+            return blocks[day]![2] + " Block"
+        case 4:
+            return "Lunch"
+        case 5:
+            return blocks[day]![3] + " Block"
+        case 6:
+            return blocks[day]![4] + " Block"
+        default:
+            return "e"
+        }
+    }
+    
     var body: some View {
-        VStack{
-            var block = 0 //the block number: how would we retrieve it?
-        Text("Scheduler").font(.title2).fontWeight(.bold).multilineTextAlignment(.center)
-            Toggle(meetingOrAssessment(), isOn: $hasAs[cycleDay]![block])
-            if hasAs() {
-                hasAs[cycleDay]![block] = true
+        VStack {
+            Text("Add Event").font(.title2).fontWeight(.bold).multilineTextAlignment(.center).padding(.bottom, 5)
+            Text(getOffsetDate())
+            Text("Day \(day), \(getPeriod(blockNum: block))")
+            Divider().padding(.vertical, 5)
+            if meetingOrAssessment() == "Meeting" {
+                Button(action: {
+                    isEvent = true
+                    // *** Schedule Meeting Notification ***
+                    let content = UNMutableNotificationContent()
+                    // TODO: pass in values here; see ScheduleNotificationView for where the values will be displayed
+                    content.title = "e"
+                    content.subtitle = "e"
+                    content.sound = UNNotificationSound.default
+                    content.body = "e"
+                    content.categoryIdentifier = "event"
+
+                    let category = UNNotificationCategory(identifier: "event", actions: [], intentIdentifiers: [], options: [])
+                    UNUserNotificationCenter.current().setNotificationCategories([category])
+                    
+                    // TODO: use the cycle day and block instance variables, as well as any methods we have, to calculate the components for the date that we need to trigger the notification
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: DateComponents(), repeats: false)
+
+                    // TODO: somehow construct a unique identifier string from the date and class or block information; do it in such a way as to ensure that any other meeting we schedule into some other block cannot possibly have the same string
+                    let request = UNNotificationRequest(identifier: "[insert id here]", content: content, trigger: trigger)
+
+                    // add our notification request
+                    UNUserNotificationCenter.current().add(request)
+                }, label: { Text("Schedule Meeting").fontWeight(.medium) })
             } else {
-                hasAs[cycleDay]![block] = false
+                Button(action: {
+                    isEvent = true
+                    // *** Schedule Assessment Notification ***
+                    let content = UNMutableNotificationContent()
+                    // TODO: pass in values here; see ScheduleNotificationView for where the values will be displayed
+                    content.title = "e"
+                    content.subtitle = "e"
+                    content.sound = UNNotificationSound.default
+                    content.body = "e"
+                    content.categoryIdentifier = "event"
+
+                    let category = UNNotificationCategory(identifier: "event", actions: [], intentIdentifiers: [], options: [])
+                    UNUserNotificationCenter.current().setNotificationCategories([category])
+                    
+                    // TODO: use the cycle day and block instance variables, as well as any methods we have, to calculate the components for the date that we need to trigger the notification
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: DateComponents(), repeats: false)
+
+                    // TODO: somehow construct a unique identifier string from the date and class or block information; do it in such a way as to ensure that any other assessment we schedule into some other block cannot possibly have the same string
+                    let request = UNNotificationRequest(identifier: "[insert id here]", content: content, trigger: trigger)
+
+                    // add our notification request
+                    UNUserNotificationCenter.current().add(request)
+                }, label: { Text("Input Assessment").fontWeight(.medium) })
             }
         }
     }
@@ -47,6 +142,6 @@ struct SchedulingView: View {
 
 struct SchedulingView_Previews: PreviewProvider {
     static var previews: some View {
-        SchedulingView()
+        SchedulingView(day: 4, block: 1)
     }
 }
