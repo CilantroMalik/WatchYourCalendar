8227a3afdd6bfc1985e9db626d1c0d80cb90acd6
Rohan Malik <rohan_malik@ryecountryday.org>
2022-02-05 16:40:09 -0500

    Add controller for schedule notification

---
+++ CalendarTest WatchKit Extension/ScheduleNotificationController.swift
@@ -0,0 +1,36 @@
+//
+//  ScheduleNotificationController.swift
+//  CalendarTest WatchKit Extension
+//
+//  Created by Rohan Malik on 2/5/22.
+//
+
+import WatchKit
+import SwiftUI
+import UserNotifications
+
+class ScheduleNotificationController: WKUserNotificationHostingController<ScheduleNotificationView> {
+
+    var content = UNNotificationContent()
+    
+    override var body: ScheduleNotificationView {
+        return ScheduleNotificationView(content: content)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+        content = notification.request.content
+    }
+}
