636f9adeaf457bb248102aeb24ee1f54c599c3f3
Rohan Malik <rohan_malik@ryecountryday.org>
2022-02-05 10:21:03 -0500

    Add controller for lunch notification

---
+++ CalendarTest WatchKit Extension/LunchNotificationController.swift
@@ -0,0 +1,36 @@
+//
+//  LunchNotificationController.swift
+//  CalendarTest WatchKit Extension
+//
+//  Created by Rohan Malik on 2/5/22.
+//
+
+import WatchKit
+import SwiftUI
+import UserNotifications
+
+class LunchNotificationController: WKUserNotificationHostingController<LunchNotificationView> {
+
+    var content = UNNotificationContent()
+    
+    override var body: LunchNotificationView {
+        return LunchNotificationView(content: content)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+        content = notification.request.content
+    }
+}
