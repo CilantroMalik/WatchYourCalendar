4b3f39f32bee0888a0cb5f01d2129ae2c55ad020
JackSharks05 <jack.dehaan@iCloud.com>
2022-02-14 11:18:37 -0500

    half done push

--- CalendarTest WatchKit Extension/SchedulingView.swift
+++ CalendarTest WatchKit Extension/SchedulingView.swift
@@ -8,45 +8,68 @@
 import SwiftUI
 import UserNotifications
 
-//func hasAssessment(block: Int) -> Bool {
-//    return false
-//}
-
 func getAssessmentBlock() -> Int{
     return 0//fix
 }
-
-var hasAs: [Int: [Bool]] = [
-    //  Blk 1  Clubs  Blk 2  Blk 3  Lunch  Blk 4  Blk 5
-    0: [false, false, false, false, false, false, false],
-    1: [false, false, false, false, false, false, false],
-    2: [false, false, false, false, false, false, false],
-    3: [false, false, false, false, false, false, false],
-    4: [false, false, false, false, false, false, false],
-    5: [false, false, false, false, false, false, false],
-    6: [false, false, false, false, false, false, false],
-    7: [false, false, false, false, false, false, false],
-    8: [false, false, false, false, false, false, false]
+//
+//var hasEvent: [Int: [Bool]] = [
+//    //  Blk 1  Clubs  Blk 2  Blk 3  Lunch  Blk 4  Blk 5
+//    0: [false, false, false, false, false, false, false],
+//    1: [false, false, false, false, false, false, false],
+//    2: [false, false, false, false, false, false, false],
+//    3: [false, false, false, false, false, false, false],
+//    4: [false, false, false, false, false, false, false],
+//    5: [false, false, false, false, false, false, false],
+//    6: [false, false, false, false, false, false, false],
+//    7: [false, false, false, false, false, false, false],
+//    8: [false, false, false, false, false, false, false]
+//]
+var hasEvent: [[Int: Bool]] = [
+    // January
+    [1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false, 9:false, 10:false, 11:false, 12:false, 13:false, 14:false, 15:false, 16:false, 17:false, 18:false, 19:false, 20:false, 21:false, 22:false, 23:false, 24:false, 25:false, 26:false, 27:false, 28:false, 29:false, 30:false, 31:false],
+    // February
+    [1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false, 9:false, 10:false, 11:false, 12:false, 13:false, 14:false, 15:false, 16:false, 17:false, 18:false, 19:false, 20:false, 21:false, 22:false, 23:false, 24:false, 25:false, 26:false, 27:false, 28:false],
+    // March
+    [1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false, 9:false, 10:false, 11:false, 12:false, 13:false, 14:false, 15:false, 16:false, 17:false, 18:false, 19:false, 20:false, 21:false, 22:false, 23:false, 24:false, 25:false, 26:false, 27:false, 28:false, 29:false, 30:false, 31:false],
+    // April
+    [1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false, 9:false, 10:false, 11:false, 12:false, 13:false, 14:false, 15:false, 16:false, 17:false, 18:false, 19:false, 20:false, 21:false, 22:false, 23:false, 24:false, 25:false, 26:false, 27:false, 28:false, 29:false, 30:false],
+    // May
+    [1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false, 9:false, 10:false, 11:false, 12:false, 13:false, 14:false, 15:false, 16:false, 17:false, 18:false, 19:false, 20:false, 21:false, 22:false, 23:false, 24:false, 25:false, 26:false, 27:false, 28:false, 29:false, 30:false, 31:false],
+    // June
+    [1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false]
 ]
-var numEvents: [Int: [Int]] = [ //cycle day, block #
-    0: [0, 0, 0, 0, 0, 0, 0],
-    1: [0, 0, 0, 0, 0, 0, 0],
-    2: [0, 0, 0, 0, 0, 0, 0],
-    3: [0, 0, 0, 0, 0, 0, 0],
-    4: [0, 0, 0, 0, 0, 0, 0],
-    5: [0, 0, 0, 0, 0, 0, 0],
-    6: [0, 0, 0, 0, 0, 0, 0],
-    7: [0, 0, 0, 0, 0, 0, 0],
-    8: [0, 0, 0, 0, 0, 0, 0]
+//var numEvents: [Int: [Int]] = [ //cycle day, block #
+//    0: [0, 0, 0, 0, 0, 0, 0],
+//    1: [0, 0, 0, 0, 0, 0, 0],
+//    2: [0, 0, 0, 0, 0, 0, 0],
+//    3: [0, 0, 0, 0, 0, 0, 0],
+//    4: [0, 0, 0, 0, 0, 0, 0],
+//    5: [0, 0, 0, 0, 0, 0, 0],
+//    6: [0, 0, 0, 0, 0, 0, 0],
+//    7: [0, 0, 0, 0, 0, 0, 0],
+//    8: [0, 0, 0, 0, 0, 0, 0]
+//]
+var numEvents: [[Int: Int]] = [
+    // January
+    [1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0, 25:0, 26:0, 27:0, 28:0, 29:0, 30:0, 31:0],
+    // February
+    [1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0, 25:0, 26:0, 27:0, 28:0],
+    // March
+    [1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0, 25:0, 26:0, 27:0, 28:0, 29:0, 30:0, 31:0],
+    // April
+    [1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0, 25:0, 26:0, 27:0, 28:0, 29:0, 30:0],
+    // May
+    [1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0, 25:0, 26:0, 27:0, 28:0, 29:0, 30:0, 31:0],
+    // June
+    [1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0]
 ]
 
 struct SchedulingView: View {
     var day: Int
     var block: Int
-    
     @State var isEvent = false {
         didSet {
-            hasAs[day]![block] = isEvent
+            hasEvent[day]![block] = isEvent
         }
     }
     
