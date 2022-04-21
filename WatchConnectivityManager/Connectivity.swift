001ef2354ddfe1a7f6b59116dc8eba83896207d8
Rohan Malik <rohan_malik@ryecountryday.org>
2022-04-21 15:43:10 -0400

    Create watch connectivity handler

---
+++ WatchConnectivityManager/Connectivity.swift
@@ -0,0 +1,54 @@
+//
+//  Connectivity.swift
+//  CalendarTest
+//
+//  Created by Rohan Malik on 4/20/22.
+//
+
+import Foundation
+import WatchConnectivity
+
+final class Connectivity: NSObject, ObservableObject {
+    static let shared = Connectivity()
+    @Published var data: [String: Any] = [:]
+    
+    override private init() {
+        super.init()
+        guard WCSession.isSupported() else { return }
+        WCSession.default.delegate = self
+        WCSession.default.activate()
+    }
+    
+    public func send(obj: [String: Any]) {
+        guard WCSession.default.activationState == .activated else { return }
+        #if os(watchOS)
+            guard WCSession.default.isCompanionAppInstalled else { return }
+        #else
+            guard WCSession.default.isWatchAppInstalled else { return }
+        #endif
+        WCSession.default.transferUserInfo(obj)
+        print("Initiated data transfer to watch")
+    }
+}
+
+
+extension Connectivity: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveUserInfo userInfo: [String : Any] = [:]) {
+        self.data = userInfo
+        print("Setting data from session")
+    }
+
+    #if os(iOS)
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+    }
+
+    func sessionDidDeactivate(_ session: WCSession) {
+        WCSession.default.activate()
+    }
+    #endif
+}
