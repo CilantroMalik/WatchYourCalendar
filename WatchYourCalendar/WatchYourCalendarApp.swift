31039d14d8c02f1ca5660d2daf7e19c61b89c60f
Rohan Malik <rohan_malik@ryecountryday.org>
2022-04-21 15:42:51 -0400

    Create phone app shell

---
+++ WatchYourCalendar/WatchYourCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchYourCalendarApp.swift
+//  WatchYourCalendar
+//
+//  Created by Rohan Malik on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchYourCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
