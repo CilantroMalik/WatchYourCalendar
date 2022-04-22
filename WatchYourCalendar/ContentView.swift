c3867bb2fb9b7eb4d3fdf003ae1af83bf6f05f59
Rohan Malik <rohan_malik@ryecountryday.org>
2022-04-22 09:19:47 -0400

    Un-de-jankify because destructuring is hard in strongly typed languages

--- WatchYourCalendar/ContentView.swift
+++ WatchYourCalendar/ContentView.swift
@@ -12,10 +12,10 @@ struct ContentView: View {
     @State var classes: [String] = ["", "", "", "", "", "", "", ""]
     @State var firstLunch: [Bool] = [false, false, false, false, false, false, false, false]
     
-    func blockForm(_ bl: String) -> some View {
+    func blockForm(_ bl: Strin, _ i: Int) -> some View {
         return HStack {
             Text("\(bl) Block:").padding(.leading, 15.0)
-            TextField("Class", text: $classes[0], prompt: Text("Fill in class name here"))
+            TextField("Class", text: $classes[i], prompt: Text("Fill in class name here"))
         }
     }
     
@@ -30,29 +30,29 @@ struct ContentView: View {
             Text("Please fill in your classes below.").padding(.bottom, 5)
             Text("If you have a free during a block, type 'Free'.").multilineTextAlignment(.center).padding(.bottom, 10)
             Group {
-                blockForm("A")
-                blockForm("B")
-                blockForm("C")
-                blockForm("D")
-                blockForm("E")
-                blockForm("F")
-                blockForm("G")
-                blockForm("H")
+                blockForm("A", 0)
+                blockForm("B", 1)
+                blockForm("C", 2)
+                blockForm("D", 3)
+                blockForm("E", 4)
+                blockForm("F", 5)
+                blockForm("G", 6)
+                blockForm("H", 7)
             }
             Divider().padding(.horizontal, 5)
             Text("Check each day that you have first lunch.").multilineTextAlignment(.center).padding(.bottom, 10).padding(.top, 15)
             HStack {
                 VStack {
                     Toggle(isOn: $firstLunch[0]) { Text("Day 1") }.padding(.horizontal, 35.0).toggleStyle(.switch)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 2") }.padding(.horizontal, 35.0)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 3") }.padding(.horizontal, 35.0)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 4") }.padding(.horizontal, 35.0)
+                    Toggle(isOn: $firstLunch[1]) { Text("Day 2") }.padding(.horizontal, 35.0)
+                    Toggle(isOn: $firstLunch[2]) { Text("Day 3") }.padding(.horizontal, 35.0)
+                    Toggle(isOn: $firstLunch[3]) { Text("Day 4") }.padding(.horizontal, 35.0)
                 }
                 VStack {
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 5") }.padding(.horizontal, 35.0)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 6") }.padding(.horizontal, 35.0)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 7") }.padding(.horizontal, 35.0)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 8") }.padding(.horizontal, 35.0)
+                    Toggle(isOn: $firstLunch[4]) { Text("Day 5") }.padding(.horizontal, 35.0)
+                    Toggle(isOn: $firstLunch[5]) { Text("Day 6") }.padding(.horizontal, 35.0)
+                    Toggle(isOn: $firstLunch[6]) { Text("Day 7") }.padding(.horizontal, 35.0)
+                    Toggle(isOn: $firstLunch[7]) { Text("Day 8") }.padding(.horizontal, 35.0)
                 }
             }
             Divider().padding(.vertical, 10)
