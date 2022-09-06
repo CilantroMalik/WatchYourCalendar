fc7c33bf706c0f95326a6249098645b6fc2a8695
Rohan Malik <rohan_malik@ryecountryday.org>
2022-09-06 18:28:40 -0400

    Tabbed interface + data persistence in phone app

--- whatever/ContentView.swift
+++ whatever/ContentView.swift
@@ -9,54 +9,12 @@ import SwiftUI
 import WatchConnectivity
 
 struct ContentView: View {
-    @State var classes: [String] = ["", "", "", "", "", "", "", ""]
-    @State var firstLunch: [Bool] = [false, false, false, false, false, false, false, false]
     
-    func blockForm(_ bl: String) -> some View {
-        return HStack {
-            Text("\(bl) Block:").padding(.leading, 15.0)
-            TextField("Class", text: $classes[0], prompt: Text("Fill in class name here"))
-        }
-    }
-    
-    func sendToWatch() {
-        Connectivity.shared.send(obj: ["classes": classes, "firstLunch": firstLunch])
-    }
     
     var body: some View {
-        VStack {
-            Text("WatchYourCalendar Configuration").fontWeight(.bold).font(.system(size: 20))
-            Divider().padding(.vertical, 7).padding(.horizontal, 5)
-            Text("Please fill in your classes below.").padding(.bottom, 5)
-            Text("If you have a free during a block, type 'Free'.").multilineTextAlignment(.center).padding(.bottom, 10)
-            Group {
-                blockForm("A")
-                blockForm("B")
-                blockForm("C")
-                blockForm("D")
-                blockForm("E")
-                blockForm("F")
-                blockForm("G")
-                blockForm("H")
-            }
-            Divider().padding(.horizontal, 5)
-            Text("Check each day that you have first lunch.").multilineTextAlignment(.center).padding(.bottom, 10).padding(.top, 15)
-            HStack {
-                VStack {
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 1") }.padding(.horizontal, 35.0).toggleStyle(.switch)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 2") }.padding(.horizontal, 35.0).toggleStyle(.switch)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 3") }.padding(.horizontal, 35.0).toggleStyle(.switch)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 4") }.padding(.horizontal, 35.0).toggleStyle(.switch)
-                }
-                VStack {
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 5") }.padding(.horizontal, 35.0).toggleStyle(.switch)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 6") }.padding(.horizontal, 35.0).toggleStyle(.switch)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 7") }.padding(.horizontal, 35.0).toggleStyle(.switch)
-                    Toggle(isOn: $firstLunch[0]) { Text("Day 8") }.padding(.horizontal, 35.0).toggleStyle(.switch)
-                }
-            }
-            Divider().padding(.vertical, 10)
-            Button("Send to Watch App", action: {sendToWatch()})
+        TabView {
+            RichDayView().tabItem { Label("Home", systemImage: "house.fill") }
+            WatchConfig().tabItem { Label("Config", systemImage: "gearshape.fill") }
         }
     }
 }
--- whatever/whateverApp.swift
+++ whatever/whateverApp.swift
@@ -9,6 +9,58 @@ import SwiftUI
 
 @main
 struct WatchYourCalendarApp: App {
+    init() {
+        print("initializing")
+        let ud = UserDefaults.standard
+        guard let temp = ud.array(forKey: "ZLunch") else { return }
+        for i in 0...5 {
+            ZLunch[i+1] = (temp[i] as! Int)
+        }
+        guard let temp = ud.stringArray(forKey: "classes") else { return }
+        let a = temp[0]
+        let b = temp[1]
+        let c = temp[2]
+        let d = temp[3]
+        let e = temp[4]
+        let f = temp[5]
+        let g = temp[6]
+        let h = temp[7]
+        let z1 = temp[8]
+        let z2 = temp[9]
+        classes[1] = [a, b, c, z1, z2, d]
+        classes[2] = [e, f, g, z1, z2, h]
+        classes[3] = [d, a, b, z1, z2, c]
+        classes[4] = [h, e, f, z1, z2, g]
+        classes[5] = [c, d, a, z1, z2, b]
+        classes[6] = [g, h, e, z1, z2, f]
+        guard let temp = ud.stringArray(forKey: "sports") else { return }
+        for i in 0...5 {
+            sports[i] = temp[i]
+        }
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if success { print("All set!") }
+            else if let error = error { print(error.localizedDescription) }
+        }
+        
+        UISegmentedControl.appearance().apportionsSegmentWidthsByContent = true
+        
+        guard let temp = ud.stringArray(forKey: "eventsList") else { return }
+        print("-- Before --")
+        print(EventsListObs.evList)
+        print("-- Temp -- ")
+        print(temp)
+        for str in temp {
+            let comps = str.split(separator: "-")
+            print("- Comps -")
+            print(comps)
+            print("- Block Event -")
+            print(blockEvent(String(comps[2])).toString())
+            EventsListObs.evList[Int(String(comps[0]))!][Int(String(comps[1]))!]?.append(blockEvent(String(comps[2])))
+        }
+        print("-- After --")
+        print(EventsListObs.evList)
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
